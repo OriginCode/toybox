9f8d787dcdd547cd2d09c90f7f67632de3faa787
OriginCode <origincoder@yahoo.com>
2019-03-21 22:03:11 +0800

    [owm_cityid] fix update func

--- toybox.py
+++ toybox.py
@@ -53,8 +53,11 @@ class owm_cityid:
     def update():
         d = {}
 
-        src = requests.get('http://bulk.openweathermap.org/sample/city.list.json.gz').content
-        with gzip.open(src) as f:
+        # Recommended: Manually download the file below, and put it in this folder.
+        # src = requests.get('http://bulk.openweathermap.org/sample/city.list.json.gz').content
+        # with open('./city.list.json.gz', 'w') as f:
+        #     f.write(src)
+        with gzip.open('./city.list.json.gz') as f:
             j = json.loads(f.read())
         for x in range(len(j)):
             d.update({j[x]['name']: j[x]['id']})
